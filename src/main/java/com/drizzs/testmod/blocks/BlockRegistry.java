5c421eb518241cbb4959ac88437675f41394c9c8
Drizzs <drizzs@hotmail.com>
2019-09-26 20:08:36 -0400

    Block Tutorial

---
+++ src/main/java/com/drizzs/testmod/blocks/BlockRegistry.java
@@ -0,0 +1,54 @@
+package com.drizzs.testmod.blocks;
+
+import com.drizzs.testmod.TestMod;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = TestMod.MOD_ID, bus=Mod.EventBusSubscriber.Bus.MOD)
+public class BlockRegistry
+{
+
+    public static Block basic_block;
+
+    @SubscribeEvent
+    public static void registerBlocks(final RegistryEvent.Register<Block> event)
+    {
+
+        basic_block = registerBlock(new Block((Block.Properties.create(Material.ROCK).hardnessAndResistance(0.6F).sound(SoundType.STONE))), "basic_block");
+
+    }
+
+    public static Block registerBlock(Block block, String name)
+    {
+        BlockItem itemBlock = new BlockItem(block, new Item.Properties().group(ItemGroup.MISC));
+        block.setRegistryName(name);
+        itemBlock.setRegistryName(name);
+        ForgeRegistries.BLOCKS.register(block);
+        ForgeRegistries.ITEMS.register(itemBlock);
+        return block;
+    }
+
+    public static Block registerBlock(Block block, BlockItem itemBlock, String name)
+    {
+        block.setRegistryName(name);
+        ForgeRegistries.BLOCKS.register(block);
+
+        if (itemBlock != null)
+        {
+            itemBlock.setRegistryName(name);
+            ForgeRegistries.ITEMS.register(itemBlock);
+        }
+
+        return block;
+    }
+
+
+}
