add31897967347c925c482680a8d1ee7c1073826
Drizzs <drizzs@hotmail.com>
2019-09-27 01:45:46 -0400

    Biome Creation

---
+++ src/main/java/com/drizzs/testmod/biome/BiomeRegistry.java
@@ -0,0 +1,38 @@
+package com.drizzs.testmod.biome;
+
+import com.drizzs.testmod.TestMod;
+import com.drizzs.testmod.biome.overworld.TestBiome;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@Mod.EventBusSubscriber(modid = TestMod.MOD_ID, bus= Mod.EventBusSubscriber.Bus.MOD)
+public class BiomeRegistry
+{
+
+    public static Biome testbiome;
+
+    @SubscribeEvent
+    public static void registerBiomes(final RegistryEvent.Register<Biome> event)
+    {
+        testbiome = registerBiome(new TestBiome(), BiomeManager.BiomeType.WARM , "testbiome", 1000, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.DENSE, BiomeDictionary.Type.SPOOKY);
+
+    }
+
+    public static Biome registerBiome (Biome biome, BiomeManager.BiomeType biometype, String name, int weight, BiomeDictionary.Type... types)
+    {
+        biome.setRegistryName(name);
+        ForgeRegistries.BIOMES.register(biome);
+        BiomeManager.addSpawnBiome(biome);
+        BiomeManager.addBiome(biometype, new BiomeManager.BiomeEntry(biome, weight));
+        BiomeDictionary.addTypes(biome, types);
+        System.out.println(name + "registered");
+        return biome;
+
+    }
+
+}
